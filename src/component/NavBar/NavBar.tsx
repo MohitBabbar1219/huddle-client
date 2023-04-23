dfac3b1da89f4a3b4056cc619528ddd3178f8950
mohit.babbar <mohitbabbar1219@gmail.com>
2023-04-23 15:12:47 +0530

    Fix menu items

--- src/component/NavBar/NavBar.tsx
+++ src/component/NavBar/NavBar.tsx
@@ -3,7 +3,7 @@ import React from 'react';
 const NavBar = (props: any) => {
   return (
       <nav className="navbar px-5 navbar-expand-lg navbar-light bg-light">
-        <a className="navbar-brand" href="#">Navbar</a>
+        <a className="navbar-brand" href="#">Huddle</a>
         <button className="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false"
                 aria-label="Toggle navigation">
           <span className="navbar-toggler-icon"></span>
@@ -11,16 +11,13 @@ const NavBar = (props: any) => {
         <div className="collapse navbar-collapse" id="navbarNav">
           <ul className="navbar-nav">
             <li className="nav-item active">
-              <a className="nav-link" href="#">Home <span className="sr-only">(current)</span></a>
+              <a className="nav-link" href="#">Meeting Notes</a>
             </li>
             <li className="nav-item">
-              <a className="nav-link" href="#">Features</a>
+              <a className="nav-link" href="#">Text & Communication</a>
             </li>
             <li className="nav-item">
-              <a className="nav-link" href="#">Pricing</a>
-            </li>
-            <li className="nav-item">
-              <a className="nav-link disabled" href="#">Disabled</a>
+              <a className="nav-link" href="#">Writing Helps</a>
             </li>
           </ul>
         </div>
